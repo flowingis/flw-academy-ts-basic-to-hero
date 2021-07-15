126ed22d52c31bb247a1729eff5e6f526097ccd7
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 16:03:38 +0200

    lexical this

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello from Flowing Academy");
+class Person {
+  constructor(private age: number) {}
+
+  growOlder(): void {
+    this.age++;
+  }
+
+  //   growOlder: () => void = () => {
+  //     this.age++;
+  //   };
+
+  getAge(): number {
+    return this.age;
+  }
+}
+
+const p = new Person(0);
+p.growOlder();
+console.log("Age 1", p.getAge());
+
+const growOlder = p.growOlder;
+growOlder();
+console.log("Age 2", p.getAge());
