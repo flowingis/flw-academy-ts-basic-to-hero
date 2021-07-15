70321201ecd0c0cc1365ea29d449cc8b98a37ae5
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 15:34:42 +0200

    type assert

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello from Flowing Academy");
+function loadValue(): unknown {
+  const rnd = Math.round(Math.random() * 100);
+  return Math.random() > 0.5 ? String(rnd) : rnd;
+}
+
+let value = loadValue();
+
+{
+  const trimmed = value.trim();
+}
+
+{
+  const trimmed = (value as string).trim();
+}
+
+{
+  const trimmed = (<string>value).trim(); // tsx problem
+}
+
+{
+  // best method
+  if (typeof value === "string") {
+    const trimmed = value.trim();
+  } else {
+    console.log("value is not of string type");
+  }
+}
