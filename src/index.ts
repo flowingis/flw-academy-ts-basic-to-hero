023c933d86b2c393bf2ce1095f0577d0c52874da
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 15:55:46 +0200

    promise

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello from Flowing Academy");
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const main: () => Promise<void> = async () => {
+  await delay(1000);
+  console.log("1s");
+  await delay(1000);
+  console.log("2s");
+  await delay(1000);
+  console.log("3s");
+};
+
+main();
+console.log("Done script but waiting task");
