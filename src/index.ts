24d0caa9ec97729c913fcb5742aab1c5b0a90f1a
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 17:48:52 +0200

    literal types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello from Flowing Academy");
+type Direction = "left" | "right" | "up" | "down";
+
+let direction: Direction = "left";
+// direction = "Left";
+// Type '"Left"' is not assignable to type 'Direction'.
+direction = "right";
+
+type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
+
+const rollDice = () => Math.floor(Math.random() * 6 + 1) as DiceValue;
+
+// This condition will always return 'false' since the types 'DiceValue' and '7' have no overlap.ts(2367)
+// if (rollDice() === 7) {
+//   throw new Error("Not Possible");
+// }
