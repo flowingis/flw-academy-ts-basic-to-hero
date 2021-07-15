f93cd4fcf29e0b7b4204c85ecbad4c68b76274ca
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 18:39:17 +0200

    no strinct

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log("Hello from Flowing Academy");
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(1, 2));
+console.log(add("Hello", "World"));
+
+class Point {
+  x: number;
+  y: number;
+
+  move(x: number, y: number): void {
+    this.x += p.x;
+    this.y += p.y;
+    console.log(`Point move to x: ${this.x} y: ${this.y}`);
+  }
+}
+
+const p = new Point();
+
+p.move(1, 2);
+
+type User = { name: string; age: number };
+
+const users: Array<User> = [
+  { name: "John", age: 30 },
+  { name: "Mary", age: 40 },
+];
+
+function getUserAge(name: string): number {
+  return users.find(u => u.name === name).age;
+}
+
+console.log(`User 'Bro' has ${getUserAge("Bro")}`);
