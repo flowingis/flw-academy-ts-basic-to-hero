be41a77901bbcb5d72c848be770ea0aa5856073c
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 18:15:09 +0200

    discriminated unions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello from Flowing Academy");
+type Square = {
+  kind: "square";
+  size: number;
+};
+
+type Rectangle = {
+  kind: "rectangle";
+  width: number;
+  height: number;
+};
+
+type Circle = {
+  kind: "circle";
+  radius: number;
+};
+
+type Shape = Square | Rectangle;
+
+const area = (shape: Shape): number => {
+  switch (shape.kind) {
+    case "square":
+      return shape.size * shape.size;
+    case "rectangle":
+      return shape.width * shape.height;
+    // case "circle":
+    //   return Math.PI * shape.radius * shape.radius;
+  }
+};
+
+const square: Square = { kind: "square", size: 2 };
+const rectangle: Rectangle = { kind: "rectangle", width: 2, height: 3 };
+const circle: Circle = { kind: "circle", radius: 2 };
+console.log("square", area(square));
+console.log("rectangle", area(rectangle));
+// console.log("circle", area(circle));
