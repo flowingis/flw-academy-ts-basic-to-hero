75f7bc25ca071e922b0ad84e592f65e75bd35f0a
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 15:11:02 +0200

    generics

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("Hello from Flowing Academy");
+class Queue<T> {
+  private queue: T[] = [];
+
+  push(item: T) {
+    this.queue.push(item);
+  }
+
+  pop() {
+    return this.queue.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.push(1);
+queue.push(2);
+console.log("queue pop", queue.pop());
+
+type Catalog<TProduct> = { products: TProduct[] };
+type Bike = { name: string; price: number };
+
+type BikesCatalog = Catalog<Bike>;
+
+const bikesCatalog: Catalog<Bike> = {
+  products: [
+    {
+      name: "A",
+      price: 100,
+    },
+    {
+      name: "B",
+      price: 200,
+    },
+  ],
+};
+
+console.log("bikes", bikesCatalog.products);
+
+// bikesCatalog.products.push({
+//   name: "prova",
+// });
+// Property 'price' is missing in type '{ name: string; }' but required in type 'Bike'.
