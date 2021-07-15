7f127ce72838962f3af4c599d142d78bb7542597
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 14:25:26 +0200

    functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello from Flowing Academy");
+/**
+ * Functions
+ */
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+function log(message: string): void {
+  console.log(message);
+}
+
+console.log("sum", sum(1, 2));
+log("Hello from Flowing Academy");
+
+/**
+ * Lambda
+ */
+
+type Sum = (a: number, b: number) => number;
+
+const sumLambda: Sum = (a: number, b: number) => a + b;
+let sumFn: Sum;
+
+sumFn = function (a: number, b: number): number {
+  // return value type can be inferred
+  return a + b;
+};
+
+console.log("sumLambda", sumLambda(1, 2));
+console.log("sumFn", sumFn(1, 2));
