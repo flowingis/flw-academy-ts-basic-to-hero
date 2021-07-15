70d5db6a5a5427870ee094ba5ddb521fd240a100
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 17:31:53 +0200

    union type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello from Flowing Academy");
+const padLeft = (value: string, padding: number | string): string => {
+  if (typeof padding === "number") return Array(padding + 1).join(" ") + value;
+
+  if (typeof padding === "string") return padding + value;
+
+  throw new Error(`Invalid padding! Expected number of string, got ${padding}`);
+};
+
+console.log(padLeft("foo", 3));
+console.log(padLeft("foo", "    "));
+console.log(padLeft("foo", "----"));
+// console.log(padLeft("foo", false);
