73b202da1838c9de01697f35190f6cdea81ea450
Luca Del Puppo <luca@delpuppo.net>
2021-07-15 15:19:25 +0200

    any vs unknown

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello from Flowing Academy");
+let exampleAny: any;
+
+exampleAny = 13;
+exampleAny = "string";
+
+exampleAny.allows.anything.you.can.imagine();
+let anySetBoolean: boolean = exampleAny;
+
+let exampleUnknown: unknown;
+
+exampleUnknown = 13;
+exampleUnknown = "string";
+exampleUnknown = true;
+
+exampleUnknown.trim();
+let unknownSetBoolean: boolean = exampleUnknown;
