eeb702171cf3f2126d17fab7f967ff0151eb4a43
Luca Del Puppo <luca@delpuppo.net>
2021-07-19 14:23:59 +0200

    index signatures

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello from Flowing Academy");
+type Dictionary<T> = {
+  [key: string]: T;
+};
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+type Animal = {
+  name: string;
+};
+
+const people: Dictionary<Person> = {};
+
+people["john"] = { name: "john", age: 30 };
+people["mary"] = { name: "mary", age: 40 };
+people["peter"] = { name: "peter", age: 20 };
+
+console.log("People", people);
+
+const animals: Dictionary<Animal> = {};
+
+animals["cat"] = { name: "cat" };
+animals["dog"] = { name: "dog" };
+animals["bird"] = { name: "bird" };
+
+console.log("Animals", animals);
