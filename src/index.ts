85e758587ed4727bf398c633f7a38202dfd4699b
Luca Del Puppo <luca@delpuppo.net>
2021-07-19 15:56:22 +0200

    infer and return type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello from Flowing Academy");
+type UnboxingArray<T> = T extends Array<infer Member> ? Member : T;
+
+type UnboxingArrayString = UnboxingArray<string[]>; // string
+type UnboxingArrayNumber = UnboxingArray<number[]>; // number
+type UnboxingString = UnboxingArray<string>; // string
+
+function f() {
+  return {
+    a: [1, 2, 3],
+  };
+}
+
+type ReturnTypeExample<T> = T extends (...args: unknown[]) => infer R ? R : any;
+
+type FResultTypeExample = ReturnTypeExample<typeof f>; // { a: number[]; }
+type FResultTypeStringExample = ReturnTypeExample<string>; // any
+// ReturnType is out of the box
+type FResultType = ReturnType<typeof f>; // { a: number[]; }
