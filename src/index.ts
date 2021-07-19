70df380e1dc890ea6cb07a6ee5d5e3d44a00bb58
Luca Del Puppo <luca@delpuppo.net>
2021-07-19 15:04:17 +0200

    generic constraint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello from Flowing Academy");
+type NameFields = { firstName: string; lastName: string };
+
+function addFullName<T extends NameFields>(obj: T): T & { fullName: string } {
+  return {
+    ...obj,
+    fullName: `${obj.firstName} ${obj.lastName}`,
+  };
+}
+
+const person = { firstName: "Jane", lastName: "Doe" };
+const personWithFullName = addFullName(person);
+
+console.log("Person", person.firstName, person.lastName);
+console.log(
+  "Person with full name",
+  personWithFullName.firstName,
+  personWithFullName.lastName,
+  personWithFullName.fullName
+);
