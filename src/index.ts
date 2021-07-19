199784e921bb3fafef028e1a9dda644471fc5eef
Luca Del Puppo <luca@delpuppo.net>
2021-07-19 14:56:29 +0200

    this parameter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("Hello from Flowing Academy");
+// no this parameters
+{
+  console.log("No this parameters --> start");
+  function double(): void {
+    // @ts-ignore
+    this.value = this.value * 2;
+    // @ts-ignore
+    console.log("value after double", this.value);
+  }
+
+  const x = {
+    vale: 3, // name different from value
+    double,
+  };
+
+  x.double();
+
+  console.log("Value", x.vale);
+
+  console.log("No this parameters --> end");
+}
+
+// no this parameters
+{
+  console.log("this parameters --> start");
+  function double(this: { value: number }): void {
+    this.value = this.value * 2;
+    console.log("value after double", this.value);
+  }
+
+  const x = {
+    value: 3,
+    double,
+  };
+
+  x.double();
+
+  console.log("Value", x.value);
+
+  console.log("this parameters --> end");
+}
