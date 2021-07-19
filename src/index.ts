6ee5d50bef87b272d18e29171fecf9e0aea1ef67
Luca Del Puppo <luca@delpuppo.net>
2021-07-19 16:23:30 +0200

    mapped types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello from Flowing Academy");
+// no mapped types
+{
+  type Point = {
+    x: number;
+    y: number;
+  };
+
+  type ReadOnlyPoint = {
+    readonly x: number;
+    readonly y: number;
+  };
+
+  const p: Point = { x: 1, y: 2 };
+  p.x = 3;
+  p.y = 4;
+  const rp: ReadOnlyPoint = { x: 1, y: 2 };
+  rp.x = 3;
+  rp.y = 4;
+}
+
+// with mapped types
+{
+  type Point = {
+    x: number;
+    y: number;
+  };
+
+  type ReadOnly<T> = {
+    readonly [K in keyof T]: T[K];
+  };
+
+  const p: Point = { x: 1, y: 2 };
+  p.x = 3;
+  p.y = 4;
+  const rp: ReadOnly<Point> = { x: 1, y: 2 };
+  rp.x = 3;
+  rp.y = 4;
+}
