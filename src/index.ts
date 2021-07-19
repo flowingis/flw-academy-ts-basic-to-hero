79f796f1f750c32150cb9bc5058c57f44ad241d0
Luca Del Puppo <luca@delpuppo.net>
2021-07-19 09:23:19 +0200

    optional modifier

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello from Flowing Academy");
+type Person = {
+  name: string;
+  email: string;
+  phone?: string; // string | undefined
+};
+
+const person1: Person = {
+  name: "John Stack",
+  email: "john@stack.com",
+};
+
+const person2: Person = {
+  name: "John Stack",
+  email: "john@stack.com",
+  phone: "123456789",
+};
+
+console.log("Person 1", person1);
+console.log("Person 2", person2);
