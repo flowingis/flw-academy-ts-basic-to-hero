dfea489348babd6c715c2e0d3f4e1ad14d89ed99
Luca Del Puppo <luca@delpuppo.net>
2021-07-19 15:22:25 +0200

    keyof operator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello from Flowing Academy");
+type Person = {
+  name: string;
+  age: number;
+};
+
+function logGet<Obj, Key extends keyof Obj>(obj: Obj, key: Key): Obj[Key] {
+  const value = obj[key];
+  console.log(`Getting`, key, value);
+  return value;
+}
+
+function logSet<Obj, Key extends keyof Obj>(
+  obj: Obj,
+  key: Key,
+  value: Obj[Key]
+): void {
+  console.log(`Setting`, key, value);
+  obj[key] = value;
+}
+
+const person: Person = {
+  name: "John",
+  age: 30,
+};
+
+logGet(person, "name");
+logGet(person, "age");
+logGet(person, "foo");
+
+logSet(person, "name", "Jane");
+logSet(person, "age", 40);
+logSet(person, "age", "40");
+logSet(person, "foo", "bar");
