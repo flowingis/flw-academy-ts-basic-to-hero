cb0385979236156c0078a42ce0cebfdc20c437fc
Luca Del Puppo <luca@delpuppo.net>
2021-07-19 14:12:59 +0200

    function overloading

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello from Flowing Academy");
+function reverse(value: string): string;
+function reverse<T>(array: Array<T>): Array<T>;
+function reverse<T = any>(arg: string | Array<T>): string | Array<T> {
+  if (typeof arg === "string") {
+    return arg.split("").reverse().join("");
+  }
+  return arg.slice().reverse();
+}
+
+console.log("Reverse string", reverse("Hello world"));
+console.log("Reverse array", reverse(["Hello", "Word"]));
